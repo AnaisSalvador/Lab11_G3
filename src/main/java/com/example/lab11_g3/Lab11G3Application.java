186ebaf89a8c08fcea8dd06ade1816ba4148eff4
Anaís Salvador <anaissalvadorrodriguez@gmail.com>
2023-11-03 22:01:35 -0500

    subida

---
+++ src/main/java/com/example/lab11_g3/Lab11G3Application.java
@@ -0,0 +1,13 @@
+package com.example.lab11_g3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11G3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab11G3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab11_g3/Lab11G3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab11_g3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11G3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
